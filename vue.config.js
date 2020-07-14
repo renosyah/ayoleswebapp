afae6ebdb446f2142de13f929d877f8a65c085e9
renosyah <syahputrareno975@gmail.com>
2020-07-14 12:01:07 +0700

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,6 @@ module.exports = {
         themeColor: '#0D47A1',
         msTileColor: '#ffffff',
         appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
-
-        // configure the workbox plugin
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: 'dev/sw.js',
-            // ...other Workbox options...
-        }
+        appleMobileWebAppStatusBarStyle: 'black'
     }
 }
